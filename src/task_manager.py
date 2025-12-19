2aa65e685629fb82915cae6bd934f15e2c33ae95
Sidney00131 <millenysouza74@gmail.com>
2025-12-19 14:27:16 -0300

    feat: implementar CRUD básico de tarefas

--- src/task_manager.py
+++ src/task_manager.py
@@ -3,3 +3,20 @@ class Task:
         self.title = title
         self.description = description
         self.completed = False
+
+tasks = []
+
+def create_task(title, description):
+    task = Task(title, description)
+    tasks.append(task)
+    return task
+
+def list_tasks():
+    return tasks
+
+def update_task(index, title, description):
+    tasks[index].title = title
+    tasks[index].description = description
+
+def delete_task(index):
+    tasks.pop(index)
